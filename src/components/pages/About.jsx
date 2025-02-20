6358680b9031b968292b183d628e9d193ff6bc81
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-02-20 18:39:07 +0500

    done

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ export let AboutPage = () => {
     return (<>
         <Header />
         <div className="px-4 py-0 my-1 text-center">
-            <img className="d-block mx-auto mb-4" src='/public/assets/images/logo.png' alt="Company Logo" width="200" height="200" />
+            <img className="d-block mx-auto mb-4" src={`${import.meta.env.BASE_URL}assets/images/logo.png`} alt="Company Logo" width="200" height="200" />
             <h1 className="display-5 fw-bold text-body-emphasis">About Page</h1>
             <div className="col-lg-6 mx-auto">
                 <p className="lead mb-4"><span className='fw-semibold'>CodeNest</span> – The ultimate destination for developers! Learn through in-depth tutorials, solve coding challenges, stay ahead with the latest tech trends, and collaborate on exciting projects. Whether you're a beginner or a pro, CodeNest is where innovation meets growth. Join us and level up your coding journey! </p>
